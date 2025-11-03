2f9e2873a48b7a02174d7576c6a690ea0f934e8d
aluminacaris <mmaibuk@outlook.com>
2025-11-03 12:02:45 -0300

    eu nao sei oq eu mudei eu esqueci de comittar quando eu mudei...

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,29 @@
-from typing import Union
+from enum import Enum
 from fastapi import FastAPI
-from pydantic import BaseModel 
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}  
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# @app.get("/items/{item_id}") # item_id vai ser passado pra func como argumento item_id 
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
